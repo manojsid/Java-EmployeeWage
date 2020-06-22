8a8b4e2bad3bd5baae1e9b63480e417fd3f3b3a5
manojsid <siddammanoj1998@gmail.com>
2020-06-22 16:26:38 +0530

    Monthly salary added

--- emp.java
+++ emp.java
@@ -4,24 +4,29 @@ class emp{
 		int is_Present=1;
 		int salary;
 		int wagePerHr=20;
-		int fulltime_hrs=8;
-		int parttime_hrs=4;
-		double parttime=1;         
-		double fulltime=2;
-		double absent=0;
-		double random_Check=Math.floor(Math.random() * 10) % 2;
+		int hrs=0;
+		//double parttime=1;         
+		//double fulltime=2;
+		//double absent=0;
+		//double random_Check=Math.floor(Math.random() * 10) % 2;
+		int total_working_days=20;
+		int salary_per_month=0;
 		double random_Check_2=Math.floor(Math.random() * 10) % 3;
 		int random=(int)random_Check_2;
 			switch(random){
 				case 1:
 				System.out.println("Yes...!, Employee is present and he is parttime");
-				salary=wagePerHr*parttime_hrs;
-				System.out.println("And he owned salary of Rs"+salary);
+				//salary=wagePerHr*parttime_hrs;
+				hrs=4;
+				//System.out.println("And he owned salary of Rs"+salary);
+				//salary_per_month=total_working_days*salary;
+
+
 				break;
 				case 2:
 				System.out.println("Yes...!, Employee is present and he is FULLTIME");
-				salary=wagePerHr*fulltime_hrs;
-				System.out.println("And owned salary of Rs"+salary);
+				hrs=8;
+				//System.out.println("And owned salary of Rs"+salary);
 				break;
 				default:
 				System.out.println("Employee is absent");
@@ -29,7 +34,10 @@ class emp{
 
 			}
 
-
+			salary=wagePerHr*hrs;
+			System.out.println("And he owned salary of Rs"+salary);
+			salary_per_month=total_working_days*salary;
+			System.out.println("Employee wage for month is: "+salary_per_month);
 
 
 
