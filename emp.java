ffe01b74b585d50c477b9a57a1cf0022f4dcc192
manojsid <siddammanoj1998@gmail.com>
2020-06-22 15:23:53 +0530

    Attendence added

--- emp.java
+++ emp.java
@@ -1,6 +1,16 @@
 class emp{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage caluclation using Java");
+		int is_Present=1;
+		double random_Check=Math.floor(Math.random() * 10) % 2;
+			if (random_Check==is_Present) {
+				System.out.println("Employee is present");
+			}
+	else {
+		System.out.println("Employee is absent");
+	}
+
+
 	}
 
 }
