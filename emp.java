e022e24dae1b256d09114fa3beb2acb67a1a0d22
manojsid <siddammanoj1998@gmail.com>
2020-06-22 17:00:27 +0530

    Maximum days for month added

--- emp.java
+++ emp.java
@@ -2,31 +2,32 @@ class emp{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage caluclation using Java");
 		int is_Present=1;
-		int salary;
+		int salaryp=0;
+		int salaryf=0;
 		int wagePerHr=20;
 		int hrs=0;
-		//double parttime=1;         
-		//double fulltime=2;
-		//double absent=0;
-		//double random_Check=Math.floor(Math.random() * 10) % 2;
 		int total_working_days=20;
 		int salary_per_month=0;
+		int max_working_days=20;
+		
+		for ( int i=0;i<=max_working_days;i++) {
 		double random_Check_2=Math.floor(Math.random() * 10) % 3;
-		int random=(int)random_Check_2;
+		int random=(int)random_Check_2;	
+		
+		
 			switch(random){
 				case 1:
 				System.out.println("Yes...!, Employee is present and he is parttime");
-				//salary=wagePerHr*parttime_hrs;
 				hrs=4;
-				//System.out.println("And he owned salary of Rs"+salary);
-				//salary_per_month=total_working_days*salary;
-
+				int pp=wagePerHr*hrs;
+				salaryp=pp+salaryp;
 
 				break;
 				case 2:
-				System.out.println("Yes...!, Employee is present and he is FULLTIME");
+				System.out.println("Yes...!, Employee is present and he is fulltime");
 				hrs=8;
-				//System.out.println("And owned salary of Rs"+salary);
+				int ff=wagePerHr*hrs;
+				salaryf=salaryf+ff;
 				break;
 				default:
 				System.out.println("Employee is absent");
@@ -34,11 +35,14 @@ class emp{
 
 			}
 
-			salary=wagePerHr*hrs;
-			System.out.println("And he owned salary of Rs"+salary);
-			salary_per_month=total_working_days*salary;
-			System.out.println("Employee wage for month is: "+salary_per_month);
+			
+			
 
+}
+			System.out.println("Monthly salary of parttime employee is "+salaryp);
+			System.out.println("Montly salary of fulltime employee is"+salaryf);
+			//salary_per_month=total_working_days*salary;
+			//System.out.println("Employee wage for month is: "+salary_per_month);
 
 
 
