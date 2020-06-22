da32d073698791ef73626911248afb432ea375fd
manojsid <siddammanoj1998@gmail.com>
2020-06-22 15:30:14 +0530

    Daily wage caluclated

--- emp.java
+++ emp.java
@@ -2,9 +2,14 @@ class emp{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage caluclation using Java");
 		int is_Present=1;
+		int salary;
+		int wagePerHr=20;
+		int fulltime=8;
 		double random_Check=Math.floor(Math.random() * 10) % 2;
 			if (random_Check==is_Present) {
 				System.out.println("Employee is present");
+				salary=wagePerHr*fulltime;
+				System.out.println(salary);
 			}
 	else {
 		System.out.println("Employee is absent");
