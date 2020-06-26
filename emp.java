4f6e0666b93fb581f144aacf1fa86287449ffa74
manojsid <siddammanoj1998@gmail.com>
2020-06-26 10:23:09 +0530

    Class Methods are used to compute the wage

--- emp.java
+++ emp.java
@@ -1,79 +1,38 @@
 class emp{
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee wage caluclation using Java");
-		int wagePerHr=20;
-		int hrs=0;
-		int total_working_days=20;
-		int salary=0;
-		int max_working_days=20;
-		int max_working_hrs=0;
-		int total_salary=0;
-		
-		for ( int i=0;i<=max_working_days;i++) {
-		if (max_working_hrs<100) {
-			
-		
+	public static final int Is_Parttime=1;
+	public static final int Is_Fulltime=2;
+	public static final int wagePerHr=20;
+	public static final int number_of_working_days=20;
+	public static final int max_hrs_in_month=100;
+
+		public static int employee_comp(){
+		int hrs=0, total_hrs=0, totalWorkingDays=0;
+		while (total_hrs <= max_hrs_in_month && totalWorkingDays < number_of_working_days){
+		totalWorkingDays++;
 		double random_Check_2=Math.floor(Math.random() * 10) % 3;
 		int random=(int)random_Check_2;	
-		
-		
-			switch(random){
-				case 1:
-				System.out.println("Yes...!, Employee is present and he is parttime");
+		switch(random){
+				case Is_Parttime:
 				hrs=4;
-				salary=wagePerHr*hrs;
-				System.out.println("parttime salary of employee is: "+salary);
 				break;
-				case 2:
-				System.out.println("Yes...!, Employee is present and he is fulltime");
+				case Is_Fulltime:
 				hrs=8;
-				salary=wagePerHr*hrs;
-				System.out.println("fulltime salary of employee is: "+salary);
 				break;
 				default:
 				hrs=0;
-				salary=0;
-				System.out.println("Employee is absent");
-
-
 			}
-			total_salary=total_salary+salary;
-			System.out.println("Total salary till now: " +total_salary);
-			max_working_hrs=max_working_hrs+hrs;
-			System.out.println("Total working hours till now: "+max_working_hrs);
+			total_hrs+=hrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +hrs);
+		}
+		int totalEmpWage=total_hrs*wagePerHr;
+		System.out.println("Total Emp wage: "+totalEmpWage);
+		return totalEmpWage;
 		}
 		
+public static void main(String[] args) {
+	System.out.println("Welcome to Employee wage caluclation using Java");
+	employee_comp();
 
 }
-System.out.println("Total Wage For Employee in a month: "+ total_salary);
-			//System.out.println("Employee wage for month is: "+salary_per_month);
-
-
-
-				/*if (random_Check==is_Present) {
-				//System.out.println("Employee is present");
-				if (random_Check_2==parttime) {
-					System.out.println("Yes...!, Employee is present and he is parttime");
-					salary=wagePerHr*parttime_hrs;
-					System.out.println("And he owned salary of Rs"+salary);
-				}
-				else if (random_Check_2==fulltime) {
-				System.out.println("Yes...!, Employee is present and he is FULLTIME");
-				salary=wagePerHr*fulltime_hrs;
-				System.out.println("And owned salary of Rs"+salary);
-				}	
-
-			else{
-				System.out.println("absent");
-				} 
-
-				}
-				
-	else {
-		System.out.println("Employee is absent");
-	}*/
-
-
-	}
-
 }
+
