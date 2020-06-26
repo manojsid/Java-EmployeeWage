394ab1e96a141dd0604649fee081e11b05bd84ed
manojsid <siddammanoj1998@gmail.com>
2020-06-26 14:44:15 +0530

    Multiple companies are added to method

--- emp.java
+++ emp.java
@@ -1,11 +1,11 @@
 class emp{
 	public static final int Is_Parttime=1;
 	public static final int Is_Fulltime=2;
-	public static final int wagePerHr=20;
-	public static final int number_of_working_days=20;
-	public static final int max_hrs_in_month=100;
+	//public static final int wagePerHr=20;
+	//public static final int number_of_working_days=20;
+	//public static final int max_hrs_in_month=100;
 
-		public static int employee_comp(){
+		public static int employee_comp(String company, int wagePerHr, int number_of_working_days, int max_hrs_in_month){
 		int hrs=0, total_hrs=0, totalWorkingDays=0;
 		while (total_hrs <= max_hrs_in_month && totalWorkingDays < number_of_working_days){
 		totalWorkingDays++;
@@ -25,13 +25,14 @@ class emp{
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +hrs);
 		}
 		int totalEmpWage=total_hrs*wagePerHr;
-		System.out.println("Total Emp wage: "+totalEmpWage);
+		System.out.println("Total Emp wage for Company: " +company+" is: "+totalEmpWage);
 		return totalEmpWage;
 		}
-		
+
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee wage caluclation using Java");
-	employee_comp();
+	employee_comp("Dmart", 20, 2, 10);
+	employee_comp("Reliance", 10, 4, 20);
 
 }
 }
